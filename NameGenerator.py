6814d35d8b482b99d3d03022345ded913ec9d833
Mauro Marin <104025459+mauromarme@users.noreply.github.com>
2022-04-19 23:16:24 -0500

    Add files via upload
    #Name Generator using concatenation of 2 different input variables.
    print('Welcome to "Make the best name ever" online generator')
    city_name = input("What's the name of the city you grew up in?\n")
    pet_name = input("What's the name of your pet?\n")
    print("The best name that you can choose could be" + " " + city_name + " " + pet_name)

---
+++ NameGenerator.py
@@ -0,0 +1,5 @@
+#Name Generator using concatenation of 2 different input variables.
+print('Welcome to "Make the best name ever" online generator')
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("The best name that you can choose could be" + " " + city_name + " " + pet_name)
